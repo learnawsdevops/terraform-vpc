05bd9f6d51b6fc6419ebd5b6eceb55f3e4feae54
hitesh <ontedduvenkatahiteshkumar.reddy@clarivate.com>
2025-02-19 15:37:44 +0530

    entire vpc configuration

--- output.tf
+++ output.tf
@@ -7,3 +7,15 @@ output "default_vpc_id" {
   value = data.aws_vpc.default.id
 
 }
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
